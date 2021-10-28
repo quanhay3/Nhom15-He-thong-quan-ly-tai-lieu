60761ebc2a7612d35808b2de48ead39a99339615
netpro <quanganhnguyen2382001@gmail.com>
2021-10-28 17:16:36 +0700

    sua index

--- index.php
+++ index.php
@@ -18,7 +18,8 @@
     <div class="col-12 col-lg-3">
 		<div class="card">
 			<div class="card-body">
-				<div class="d-grid"> <a href="javascript:;" class="btn btn-primary">+ Add File</a>
+				<a href="http://localhost:81/nhom15-He-thong-quan-ly-tai-lieu/admin/login.php">Đăng nhập</a>
+				<div class="d-grid"> <a href="javascript:;" class="btn btn-primary">Welcome</a>
 				</div>
 				<h5 class="my-3">My Drive</h5>
 				<div class="fm-menu">
