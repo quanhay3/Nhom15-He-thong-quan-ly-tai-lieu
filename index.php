f131ffb0a6d018f3c6cb88858146d7cc85e35aee
netpro <quanganhnguyen2382001@gmail.com>
2021-10-30 17:40:08 +0700

    sua index

--- index.php
+++ index.php
@@ -16,21 +16,21 @@
 <div class="row-nav-menu">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Trang Chủ</a>
+                    <a class="navbar-brand" href="http://localhost:81/Nhom15-He-thong-quan-ly-tai-lieu/">Trang Chủ</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="">Thống Kê</a>
+                        <a class="nav-link active" aria-current="page" href="">Giới Thiệu</a>
                         </li>
                         <li class="nav-item">
                         <a class="nav-link" href=""></a>
                         </li>
                         <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Quản lý Tài khoản
+                            Danh mục
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="#">Action</a></li>
