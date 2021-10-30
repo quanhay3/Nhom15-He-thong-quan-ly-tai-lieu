812db37b669026499baaa23dc5b5ab70f7892e63
manhchien49011 <cdo4700@gmail.com>
2021-10-30 14:14:34 +0700

    done login

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
-﻿<html>
+﻿<?php
+session_start();
+$_SESSION['username'] ="";
+$_SESSION['role']="";
+?>
+<html>
 <head>
 	<title>Đăng nhập</title>
 	<meta charset="utf-8">
@@ -49,6 +54,44 @@ margin-left:50px;
 </style>
 </head>
 <body>
+<?php
+	//Gọi file connection.php
+	require_once("connection.php");
+	// Kiểm tra nếu người dùng đã ân nút đăng nhập thì mới xử lý
+	if (isset($_POST["btn_submit"])) {
+		// lấy thông tin người dùng
+		$username = $_POST["username"];
+		$password = md5($_POST["password"]);
+		//làm sạch thông tin, xóa bỏ các tag html, ký tự đặc biệt 
+		//mà người dùng cố tình thêm vào để tấn công theo phương thức sql injection
+		$username = strip_tags($username);
+		$username = addslashes($username);
+		$password = strip_tags($password);
+		$password = addslashes($password);
+		if ($username == "" || $_POST['password'] =="") {
+						$chuoi="<script>";
+						$chuoi=$chuoi."alert('username hoặc password bạn không được để trống!')"."</script>";
+						echo"$chuoi";
+		}else{
+			$sql = "SELECT * from users where username = '$username' and password = '$password' ";
+			$query = mysqli_query($conn,$sql);
+			$num_rows = mysqli_num_rows($query);
+			if ($num_rows==0) {
+				$chuoi="<script>";
+						$chuoi=$chuoi."alert('tên đăng nhập hoặc mật khẩu không đúng !')"."</script>";
+						echo"$chuoi";
+			}else{
+				//tiến hành lưu tên đăng nhập vào session để tiện xử lý sau này
+				$_SESSION['username'] = $username;
+				while($a=mysqli_fetch_array($query))
+				{$_SESSION['role']=$a[4];}
+                // Thực thi hành động sau khi lưu thông tin vào session
+                // tiến hành chuyển hướng trang index.php
+                header('Location:http://localhost:81/Nhom15-He-thong-quan-ly-tai-lieu/main/index.php ');
+			}
+		}
+	}
+?>
 </body>
 <body id="body-color"> 
     <div id="Sign-In"> 
