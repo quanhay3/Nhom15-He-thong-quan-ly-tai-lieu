7d9bb4e97978f865838a35a3a3cff092daf5c7d6
netpro <quanganhnguyen2382001@gmail.com>
2021-10-31 15:22:39 +0700

    upload form

---
+++ admin/upload-index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class ="container">
+        <div class="row">
+            <form action="upload-index.php" method="post" encytype="multipart/form-data">
+            <h2>Upload Documents</h2>
+            <input type="file" name="my file"><br>
+            <button type ="submit" name="save">Upload</button>
+        </div>
+    </div>
+</body>
+</html>
