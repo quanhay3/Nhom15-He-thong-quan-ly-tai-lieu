0379f4face579bf8a26a07ce184977cd90fc300b
manhchien49011 <cdo4700@gmail.com>
2021-11-02 15:49:48 +0700

    .

--- admin/process-register.php
+++ admin/process-register.php
@@ -1,7 +1,4 @@
 <?php
-    
- 
-    
     // Nhận giá trị từ FORM register gửi sang:
     $username   = $_POST['username'];
     $email      = $_POST['email'];
@@ -13,14 +10,14 @@
     include('../config/connect.php');
 
     // Bước 02: Thực hiện các truy vấn
-    // 2.1 - Kiểm tra Email này đã tồn tại chưa?
-   // $sql_1 = "SELECT * FROM user WHERE username='$username' and email='$email'";
-    //$result_1 = mysqli_query($conn,$sql_1);
+    //2.1 - Kiểm tra Email này đã tồn tại chưa?
+    $sql_1 = "SELECT * FROM user WHERE username='$username' and email='$email'";
+    $result_1 = mysqli_query($conn,$sql_1);
 
-    //if(mysqli_num_rows($result_1) > 0){
-      //  $value='existed';
-       // header("Location:register.php?response=$value");
-    //}else{
+    if(mysqli_num_rows($result_1) > 0){
+        $value='existed';
+        header("Location:register.php?response=$value");
+    }else{
         // 2.2 - Nếu ko tồn tại thì mới LƯU
         // Băm mật khẩu
       //  $pass_hash = password_hash($pass1, PASSWORD_DEFAULT);
@@ -30,5 +27,5 @@
         $result_2 = mysqli_query($conn,$sql_2); //Vì lệnh thực hiện là INSERT: kết quả trả về của $result_2 là SỐ BẢN GHI CHÈN THÀNH CÔNG (SỐ NGUYÊN)
         // Instantiation and passing `true` enables exceptions
         header('location:login.php');
-  //  }
+  }
 ?>
