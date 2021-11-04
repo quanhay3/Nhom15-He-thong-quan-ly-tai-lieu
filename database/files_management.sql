eba42772233f035ec5eb685f5f8150a32eacc99a
quanhay3 <quanhay23@gmail.com>
2021-11-04 13:35:00 +0700

    database

--- database/files_management.sql
+++ database/files_management.sql
@@ -1,1 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 04, 2021 lúc 07:26 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `files_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `documents`
+--
+
+INSERT INTO `documents` (`id`, `name`, `size`) VALUES
+(1, 'ádfasdcsacdsac.docx', 0),
+(2, 'ádfasdcsacdsac.docx', 0),
+(3, 'ádfasdcsacdsac.docx', 11935),
+(4, '12324135345.docx', 11918),
+(5, 'Bài tập Java số 2.pdf', 596832),
+(6, '2021_Web Programming Projects.pdf', 299057),
+(7, 'Dethigiuaky_2021.pdf', 136277),
+(8, 'screenshot xoa.png', 241014),
+(9, 'Report_2021.docx', 794045),
+(0, 'tim hieu ve SEO.docx', 14697),
+(0, 'tim hieu ve SEO.docx', 14697),
+(0, 'matmacodien.docx', 246202),
+(0, 'Chương 1.docx', 21971);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `information_user`
+--
+
+CREATE TABLE `information_user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `information_user`
+--
+
+INSERT INTO `information_user` (`id`, `first_name`, `last_name`, `phone`, `email`, `location`) VALUES
+(1, 'Nguyễn', 'Phụ Quân', 982268313, 'quanhay23@gmail.com', 'Đình Bảng- Từ Sơn- Bắc Ninh'),
+(6, '', '', 0, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`userid`, `username`, `email`, `password`, `level`) VALUES
+(1, 'admin', '123@gmail.com', '123', 1),
+(9, 'zxc', 'zxc@gmail.com', '123', 1),
+(10, 'fsa', 'fsa@gmail.com', '123', 1),
+(11, 'uio', 'uio@gmail.com', '123', 1),
+(12, 'bcv', 'bcv@gmail.com', '123', 1),
+(45, 'quanhay3', 'quanhay23@gmail.com', 'quanhay3', 0),
+(51, 'quan', 'quanhay3@gmail.com', '$2y$10$QouDLBWrYNUNL.m9wbQniO6zynQvJ6jm21MbwnojoPBslQ4i.qVqG', 0);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `information_user`
+--
+ALTER TABLE `information_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `information_user`
+--
+ALTER TABLE `information_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
