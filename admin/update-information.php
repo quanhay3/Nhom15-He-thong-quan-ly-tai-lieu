5e0e5f531fe43c3dffb567695b1ee49d0944b206
netpro <quanganhnguyen2382001@gmail.com>
2021-11-05 15:24:12 +0700

    fix

--- admin/update-information.php
+++ admin/update-information.php
@@ -64,8 +64,6 @@ if(isset($_GET['response'])){
     </div>
     <div class="row">
   		<div class="col-sm-3"><!--left col-->
-              
-
       <div class="text-center">
         <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
         <h6>Upload a different photo...</h6>
